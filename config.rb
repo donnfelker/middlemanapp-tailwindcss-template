0dd741d47e71b7a7e17bd0d8711cf6e10280c77d
Donn Felker <donn@donnfelker.com>
2022-07-20 08:35:13 -0400

    Adding tailwind css support

--- config.rb
+++ config.rb
@@ -5,6 +5,12 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
+activate :external_pipeline,
+  name: :tailwind,
+  command: "npx tailwindcss -i ./source/stylesheets/site.css -o ./dist/stylesheets/site.css #{"--watch" unless build?}",
+  latency: 2,
+  source: "./dist/"
+
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./source/**/*.{html,erb}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
