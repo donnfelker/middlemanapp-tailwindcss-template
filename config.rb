8627070455f7f5ef6b8d78bfdae5a0026f08904a
Donn Felker <donn@donnfelker.com>
2023-07-17 21:54:55 -0400

    Upgrade middleman, remove auto-prefixer as Tailwind does this for us.

--- config.rb
+++ config.rb
@@ -1,10 +1,6 @@
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
-activate :autoprefixer do |prefix|
-  prefix.browsers = "last 2 versions"
-end
-
 activate :external_pipeline,
   name: :tailwind,
   command: "npx tailwindcss -i ./source/stylesheets/site.css -o ./dist/stylesheets/site.css #{"--watch" unless build?}",
